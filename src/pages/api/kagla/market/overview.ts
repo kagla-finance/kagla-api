12af70c8227b1e73267713e821946ba11be491d6
qudacki <touka.nt.gh@gmail.com>
2022-04-21 03:57:13 +0900

    add marketoverview

---
+++ src/pages/api/kagla/market/overview.ts
@@ -0,0 +1,39 @@
+import { poolInfoService } from 'src/factory'
+import { asHandler, cacheControl } from 'src/utils/api'
+
+/**
+ * @swagger
+ * /api/kagla/market/overview:
+ *   get:
+ *     tags:
+ *       - Kagla
+ *     description: Returns Market Overview
+ *     responses:
+ *       200:
+ *         description: MarketOverview
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 blockNumber:
+ *                   type: string
+ *                 market:
+ *                   $ref: '#/components/schemas/MarketOverview'
+ *               required:
+ *                 - blockNumber
+ *                 - market
+ *       500:
+ *         description: Unexpected error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
+const handler = asHandler(poolInfoService().getMarketOverView, {
+  headers: {
+    'Cache-Control': cacheControl(3600, 7200),
+  },
+})
+
+export default handler
