ab22ee9c33ed3c9d3c73af7ae0d951eea8ac75af
qudacki <qudacki@gmail.com>
2022-10-01 01:09:11 +0900

    fix type error

--- src/utils/coin.ts
+++ src/utils/coin.ts
@@ -1,9 +1,9 @@
 import { OVERWRITE_COINS } from 'src/constants'
 import { Coin } from 'src/models/coin'
 
-export const overwriteCoins = (coins: Coin[]): Coin[] =>
+export const overwriteCoins = <T extends Coin>(coins: T[]): T[] =>
   coins.map((coin) => {
     const overwrite = OVERWRITE_COINS[coin.address]
     if (!overwrite) return coin
-    return { ...coin, ...overwrite } as Coin
+    return { ...coin, ...overwrite } as T
   })
